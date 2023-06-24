de590ecb230b7f8fd7dc3055a5ec7c6bcecdc9bc
xiaohong <3436605724@qq.com>
2023-06-24 18:18:51 +0800

    feat: add account submission feature

---
+++ src/views/account/account.js
@@ -0,0 +1,12 @@
+const passwordInput = document.querySelector('.pa');
+const togglePasswordButton = document.querySelector('.one>img');
+
+togglePasswordButton.addEventListener('click', function() {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    togglePasswordButton.src = 'https://www-static.chinacdn.starbucks.com.cn/prod/assets/icons/icon-hide-password.svg';
+  } else {
+    passwordInput.type = 'password';
+    togglePasswordButton.src = 'https://www-static.chinacdn.starbucks.com.cn/prod/assets/icons/icon-show-password.svg';
+  }
+});
